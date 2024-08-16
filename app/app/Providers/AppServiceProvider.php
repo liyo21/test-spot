26a2eb37ab5cb4ebf5ae1829001b24906032e5d3
Eliyonai Molero <55292391+liyo21@users.noreply.github.com>
2024-08-15 23:09:29 -0400

    Update AppServiceProvider.php

--- app/app/Providers/AppServiceProvider.php
+++ app/app/Providers/AppServiceProvider.php
@@ -19,6 +19,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        resolve(\Illuminate\Routing\UrlGenerator::class)->forceScheme('https');
+
+        parent::boot();
     }
 }
