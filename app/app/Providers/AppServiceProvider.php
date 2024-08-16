ed0099547ea84307520f4c3c18eb0802941223a1
Eliyonai Molero <55292391+liyo21@users.noreply.github.com>
2024-08-15 23:15:07 -0400

    Update AppServiceProvider.php

--- app/app/Providers/AppServiceProvider.php
+++ app/app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,7 +21,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if (App::environment() === "testing") {
+        if (App::environment() !== "local") {
             URL::forceScheme("https");
         }
     }
